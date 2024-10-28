6f10aa89c4ac4e5d566270dd6e494cc67988ea06
Ujjawal Soni <ujjawaldream3@gmail.com>
2024-10-28 23:36:19 +0530

    Added Token functionality

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Person=require('./../models/Person');
-const {jwtAuthMiddleware,generateToken}=require('./../jwt');
+const Person = require("./../models/Person");
+const { jwtAuthMiddleware, generateToken } = require("./../jwt");
 // post route to add a person
 router.post("/signup", async (req, res) => {
   try {
-    const data = req.body; //Assuming the request body contains the person data
+                                                                             const data = req.body; //Assuming the request body contains the person data
 
     // Create a new Person document using the Mongoose model
     const newPerson = new Person(data);
@@ -13,81 +13,67 @@ router.post("/signup", async (req, res) => {
     // Save the new person to the database
     const response = await newPerson.save();
     console.log("Data saved");
-    const payload=
-    {
+    const payload = {
       id: response.id,
-      username: response.username
-    }
-    console.log('Payload is ',JSON.stringify(payload));
-    const token=generateToken(payload);
-    console.log("Token is: ",token);
-    res.status(200).json({response: response,token:token});
-  } 
-  catch (err) 
-  {
+      username: response.username,
+    };
+    console.log("Payload is ", JSON.stringify(payload));
+    const token = generateToken(payload);
+    console.log("Token is: ", token);
+    res.status(200).json({ response: response, token: token });
+  } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal server Error" });
   }
 });
 
 // Login Route
-router.post("/login",async(req,res)=>
-{
-  try 
-  {
+router.post("/login", async (req, res) => {
+  try {
     // Extract username and password from request body
-    const {username,password} = req.body;
+    const { username, password } = req.body;
 
     // Find the user by username
-    const user = await Person.findOne({username: username});
+    const user = await Person.findOne({ username: username });
 
     // if user does not exists or password does not match, return error
-    if(!user || !(await user.comparePassword(password)))
-    {
-      return res.status(401).json({error:"Invalid username or password"});
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
 
     // If user found, generate token
-    const payload={
+    const payload = {
       id: user.id,
-      username: user.username
-    }
+      username: user.username,
+    };
     const token = generateToken(payload);
 
     // return token as response -- This will retunrn token in json format and frontend team will manage after that
-    res.json({token});
-  } 
-  catch (error) 
-  {
+    res.json({ token });
+  } catch (error) {
     console.log(error);
-    res.status(500).json({error: "Internal Server Error"});
+    res.status(500).json({ error: "Internal Server Error" });
   }
-})
+});
 
 // Profile route
-router.get('/profile',jwtAuthMiddleware,async(req,res)=>
-{
-  try 
-  {
-    const userData=req.user;
-    console.log("User Data: ",userData);
-// Doubt
-    const userID=userData.id;
+router.get("/profile", jwtAuthMiddleware, async (req, res) => {
+  try {
+    const userData = req.user;
+    console.log("User Data: ", userData);
+    // Doubt
+    const userID = userData.id;
     const user = await Person.findById(userID);
 
-    res.status(200).json({user});
-  } 
-  catch (error) 
-  {
+    res.status(200).json({ user });
+  } catch (error) {
     console.log(error);
-    res.status(500).json({error: "Internal Server Error"});
+    res.status(500).json({ error: "Internal Server Error" });
   }
-})
-
-
+});
 
 // GET method to get the person
-router.get("/",jwtAuthMiddleware, async (req, res) => {
+router.get("/", jwtAuthMiddleware, async (req, res) => {
   try {
     const data = await Person.find();
     console.log("Data fetched");
@@ -115,49 +101,47 @@ router.get("/:workType", async (req, res) => {
   }
 });
 
-router.put('/:id',async(req,res)=>
-{
-    try {
-        const personId=req.params.id;
-        const updatedPersonData=req.body;
+router.put("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id;
+    const updatedPersonData = req.body;
 
-        const response=await Person.findByIdAndUpdate(personId,updatedPersonData,{
-            new: true,
-            runValidators: true
-        })
+    const response = await Person.findByIdAndUpdate(
+      personId,
+      updatedPersonData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
-        if(!response)
-        {
-            console.log("Wrong id");
-            return res.status(404).json({error:"Person not found"});
-        }
-        console.log("Data Updated");
-        res.status(200).json(response);
-    } 
-    catch (error) 
-    {
-        console.log(error);
-        res.status(500).json({ error: "Internal server Error" });
+    if (!response) {
+      console.log("Wrong id");
+      return res.status(404).json({ error: "Person not found" });
     }
-})
+    console.log("Data Updated");
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+});
 
-router.delete('/:id',async (req,res)=>
-{
-    try {
-        const personId=req.params.id;
+router.delete("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id;
 
-        const response=await Person.findByIdAndDelete(personId);
-        if(!response)
-            {
-                console.log("Wrong id");
-                return res.status(404).json({error:"Person not found"});
-            }
-        console.log("Person Deleted");
-        res.status(200).json({message:'Person deleted successfully'});
-    } catch (error) {
-        onsole.log(error);
-        res.status(500).json({ error: "Internal server Error" });
+    const response = await Person.findByIdAndDelete(personId);
+    if (!response) {
+      console.log("Wrong id");
+      return res.status(404).json({ error: "Person not found" });
     }
-})
+    console.log("Person Deleted");
+    res.status(200).json({ message: "Person deleted successfully" });
+  } catch (error) {
+    onsole.log(error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+});
 
-module.exports=router;
+module.exports = router;
